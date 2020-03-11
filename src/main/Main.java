6a0ffaab3417e2bb5cc63ff67de481b0fac0d2f5
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-11 19:35:49 +0530

    add switch case to make a user-interactive menu-driven program

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import myQueue.MyQueue;
+import node.Node;
 
 import java.util.Scanner;
 
@@ -8,5 +9,48 @@ public class Main {
     public static void main(String[] args) {
         MyQueue myQueue = new MyQueue();
         Scanner scanner = new Scanner(System.in);
+        myQueue.printMenu();
+        int choice = -1;
+        while (choice != 0) {
+            System.out.println("Enter choice : ");
+            try {
+                choice = Integer.parseInt(scanner.nextLine().trim());
+            } catch (NumberFormatException nfe) {
+                System.out.println("Invalid input");
+                continue;
+            }
+            switch (choice) {
+                case 0:
+                    break;
+                case 1:
+                    System.out.println("Enter element to be inserted");
+                    try {
+                        int element = Integer.parseInt(scanner.nextLine().trim());
+                        myQueue.enqueue(new Node(element));
+                    } catch (NumberFormatException nfe) {
+                        System.out.println("Invalid input");
+                        continue;
+                    }
+                    break;
+                case 2:
+                    myQueue.displayQueue();
+                    break;
+                case 3:
+                    myQueue.dequeue();
+                    break;
+                case 4:
+                    myQueue.deleteQueue();
+                    break;
+                case 5:
+                    myQueue.printMenu();
+                    break;
+                default:
+                    System.out.println("Please enter valid choice");
+            }
+        }
+
+        scanner.close();
+
     }
 }
+
