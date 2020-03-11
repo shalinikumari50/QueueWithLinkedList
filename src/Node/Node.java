19e28fe9d4765dfc6a9b395a82c8430b65386962
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-11 19:09:07 +0530

    add getter and setter methods

--- src/Node/Node.java
+++ src/Node/Node.java
@@ -7,4 +7,20 @@ public class Node {
     public Node(int data) {
         this.data = data;
     }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
