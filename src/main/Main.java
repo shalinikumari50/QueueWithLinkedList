d4ef77b08b85d948a4c78d3edefd5a9e3f76f243
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-11 19:28:35 +0530

    add object:scanner of class:Scanner

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,8 +2,11 @@ package main;
 
 import myQueue.MyQueue;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         MyQueue myQueue = new MyQueue();
+        Scanner scanner = new Scanner(System.in);
     }
 }
